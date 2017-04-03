9e48e1b45ad97aa1e75c2c9b38b9f6e8593df789
App Academy Student <student@appacademy.io>
2017-04-03 14:06:34 -0700

    Created class files and class skeletons

---
+++ chess/board.rb
@@ -0,0 +1,45 @@
+require_relative 'piece'
+
+class Board
+  def initialize
+
+  end
+
+  def to_s
+
+  end
+
+  def [](pos)
+
+  end
+
+  def []=(pos)
+
+  end
+
+  def dup
+
+  end
+
+  def move_piece(color, from_pos, to_pos)
+
+  end
+
+  def move_piece!(from_pos, to_pos)
+
+  end
+
+  def checkmate
+
+  end
+
+  protected
+
+  def find_king(color)
+
+  end
+
+  def make_starting_grid
+
+  end
+end
